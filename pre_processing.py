848f0ab4b811db97c701df39ff1e5202714e4c1f
Duc <duc.ha@web.de>
2026-04-30 17:02:13 +0200

    Add Feature Creation, Feature Selection and Encoding

--- pre_processing.py
+++ pre_processing.py
@@ -1,10 +1,27 @@
 import pandas as pd
 
-def pre_processing(raw_data):
-    raw_data = pd.read_csv(raw_data) 
+def transform(raw_data):
+    ### Delete Missing Data
+    raw_data = raw_data.dropna()
     
     
-    return processed_data
+    ### Feature Creation
+    raw_data["gender"] = raw_data["gender"].replace({"male":0, "female":1})
+    
+    raw_data["social_interaction_level"] = raw_data["social_interaction_level"].replace({"low":0, "medium":1, "high":2})
+    
+    raw_data = pd.get_dummies(raw_data, columns=["platform_usage"], drop_first=True)
+    
+    ### Feature Selection based on Correlation Heapmap
+    raw_data = raw_data.drop(columns='age')
+    raw_data = raw_data.drop(columns='addiction_level')
+    raw_data = raw_data.drop(columns='screen_time_before_sleep')
+    raw_data = raw_data.drop(columns='physical_activity')
+    raw_data = raw_data.drop(columns='academic_performance')
+    
+    return raw_data
 
 if __name__ == "__main__":
-    pre_processing("Teen_Mental_Hralth_Dataset.csv")
+    raw_data = pd.read_csv("Teen_Mental_Health_Dataset.csv") 
+    processd_data = pre_processing(raw_data)
+    print(processd_data.head())
