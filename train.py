7262e83cfeec1470075c668307ef7fa8783468a3
Duc <duc.ha@web.de>
2026-04-30 17:00:44 +0200

    Update train with SMOTE

--- train.py
+++ train.py
@@ -3,15 +3,19 @@ import pandas as pd
 import pre_processing
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from collections import Counter
+import matplotlib.pyplot as plt
 
 mental_health = pd.read_csv("Teen_Mental_Health_Dataset.csv")
 
-processed_data = pre_processing(mental_health)
+processed_data = pre_processing.transform(mental_health)
 
-processed_data = pd.get_dummies(processed_data, columns=[""], drop_first=True)
 
-X = processed_data.drop()
-y = processed_data[""].values
+X = processed_data.drop("depression_label", axis=1)
+y = processed_data["depression_label"].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+smote = SMOTE(sampling_strategy='minority', random_state=42)
+X_train_sm, y_train_sm = smote.fit_resample(X_train, y_train)
